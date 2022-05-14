0350707cd8c5fd6ff5c1b9c04a2c73aec2274213
Rajaraj22 <faballeyindya@gmail.com>
2022-05-14 14:56:52 +0530

    SeleniumP4

---
+++ SeleniumCode_1stApr_23rd/src/test/java/launcher/BrowserLauncher.java
@@ -0,0 +1,21 @@
+package launcher;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BrowserLauncher {
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver1 = new FirefoxDriver();
+
+	}
+
+}
